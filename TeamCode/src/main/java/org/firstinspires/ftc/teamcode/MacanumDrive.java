e75ecdb8f9c7c3965d3ea808b610bc6395749437
morpheus221 <mxwalker44@gmail.com>
2022-09-23 10:19:15 -0400

    fixed everything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MacanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MacanumDrive.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp(name = "MacanumDrive")
 public class MacanumDrive extends OpMode {
     DcMotor frontRightMotor;
@@ -10,6 +12,7 @@ public class MacanumDrive extends OpMode {
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
     DcMotor armMotor;
+    Servo gripperServo;
 
     @Override
     public void init() {
@@ -18,6 +21,7 @@ public class MacanumDrive extends OpMode {
         backRightMotor = hardwareMap.get(DcMotor.class, "motorBackRight");
         backLeftMotor = hardwareMap.get(DcMotor.class, "motorBackLeft");
         armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        gripperServo= hardwareMap.get(Servo.class,"gripperServo");
         frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -28,14 +32,22 @@ public class MacanumDrive extends OpMode {
 
     @Override
     public void loop() {
-        double controllerY = -gamepad1.left_stick_y;
-        double controllerX = gamepad2.left_stick_x;
-        double turn = gamepad1.right_stick_y;
-        frontRightMotor.setPower(controllerX + controllerY + turn);
-        frontLeftMotor.setPower(controllerX - controllerY - turn);
-        backRightMotor.setPower(controllerX - controllerY + turn);
-        backLeftMotor.setPower(controllerX + controllerY - turn);
+        /*double controllerY = -gamepad1.left_stick_y;
+        double controllerX = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_y;*/
+        frontRightMotor.setPower(gamepad1.right_stick_y);
+        frontLeftMotor.setPower(-gamepad1.left_stick_y);
+        backRightMotor.setPower(-gamepad1.left_stick_y);
+        backLeftMotor.setPower(gamepad1.right_stick_y);
         double armMotorSpeed = 0;
+
+        if(gamepad2.x) {
+            gripperServo.setPosition(0.3);
+        }
+        if(gamepad2.y){
+            gripperServo.setPosition(0);
+
+        }
         if (gamepad1.a) {
             armMotorSpeed = 1;
             armMotor.setPower(armMotorSpeed / 2);
