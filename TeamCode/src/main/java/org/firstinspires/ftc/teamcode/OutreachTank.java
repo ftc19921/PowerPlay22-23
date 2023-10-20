a5c8c40ce22f25221301a3fb5d652fe264935faf
FTC-24260 <148502284+FTC-24260@users.noreply.github.com>
2023-10-20 11:53:58 -0400

    OutreachTank: add drone servo
     work done by @gavineng12 and @MatthewWHuang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutreachTank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutreachTank.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp()
 public class OutreachTank extends OpMode {
     DcMotor motorL;
     DcMotor motorR;
+    CRServo Drone;
     double joyLY;
     double joyRY;
 
@@ -15,6 +17,7 @@ public class OutreachTank extends OpMode {
     public void init() {
         motorL = hardwareMap.get(DcMotor.class, "Motor1");
         motorR = hardwareMap.get(DcMotor.class, "Motor2");
+        Drone = hardwareMap.get(CRServo.class, "DroneServo");
     }
     @Override
     public void loop() {
@@ -24,5 +27,10 @@ public class OutreachTank extends OpMode {
         telemetry.addData("Right stick y", joyRY);
         motorL.setPower(joyLY);
         motorR.setPower(joyRY);
+        if (gamepad1.y) {
+            Drone.setPower(1);
+        } else {
+            Drone.setPower(0);
+        }
     }
 }
