eb25aa4949a5b31756c17d551d5eed0695368319
ethananderson <eanderso2@icloud.com>
2023-10-27 11:24:56 -0400

    made andrews thang work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/andrewsIntakeMotor.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import android.transition.Slide;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
+
+public class andrewsIntakeMotor extends OpMode {
+    double motorSpeed;
+    boolean motorOn;
+    DcMotor intakeMotor;
+
+    @Override
+    public void init() {
+        intakeMotor = hardwareMap.get(DcMotor.class, "wheelyMotor");
+    }
+
+    @Override
+    public void loop() {
+
+        motorSpeed = gamepad1.right_trigger;
+
+        if (motorSpeed > 0) {
+            motorOn = true;
+        } else {
+            motorOn = false;
+        }
+        telemetry.addData("motorSpeed",motorSpeed);
+        intakeMotor.setPower(motorSpeed);
+    }
+}
